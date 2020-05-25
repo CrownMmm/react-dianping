8406ab28fbfaede43103fb77014aea113e0dae11
zhangjiaming <823600537@qq.com>
2020-05-25 18:29:48 +0800

    feat:headline组件开发完成

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Category from './components/Category'
-
+import Headline from './components/Headline'
 
 class Home extends Component {
     render() {
         return (
             <div>
-                <Category/>
+                <Category />
+                <Headline />
             </div>
         );
     }
