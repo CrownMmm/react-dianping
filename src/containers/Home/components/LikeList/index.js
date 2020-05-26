eaaa83a07ecbd8408a8ea153aceb856f43eb1aec
zhangjiaming <823600537@qq.com>
2020-05-26 22:25:45 +0800

    feat:代码优化

--- src/containers/Home/components/LikeList/index.js
+++ src/containers/Home/components/LikeList/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import LikeItem from "../LikeItem";
+import LikeItem from "../LikeItem"
 import Loading from "../../../../components/Loading"
 import "./style.css"
 
@@ -65,6 +65,8 @@ const dataSource = [
     saleDesc: "已售23976"
   }
 ];
+
+
 class LikeList extends Component {
   constructor(props) {
     super(props)
@@ -73,60 +75,61 @@ class LikeList extends Component {
       data: dataSource,
       loadTimes: 1,
     }
-    this.removeListener = false
+    this.removeListener = false;
   }
+
   render() {
-    const { data, loadTimes } = this.state
+    const {data, loadTimes} = this.state;
     return (
       <div ref={this.myRef} className="likeList">
         <div className="likeList__header">猜你喜欢</div>
         <div className="likeList__list">
           {
             data.map((item, index) => {
-              return <LikeItem key={index} data={item} />
+              return <LikeItem key={index} data={item}/>
             })
           }
         </div>
         {
           loadTimes < 3 ? (
-            <Loading />
-          ) : (
-              <a className="likeList__viewAll">
-                查看更多
-              </a>
-            )
+            <Loading/>
+          ): (
+            <a className="likeList__viewAll">
+              查看更多
+            </a>
+          )
         }
       </div>
     );
   }
 
   componentDidMount() {
-    document.addEventListener("scroll", this.handleScroll)
+    document.addEventListener("scroll", this.handleScroll);
   }
 
   componentDidUpdate() {
-    if (this.state.loadTimes >= 3 && this.removeListener) {
+    if(this.state.loadTimes >=3 && !this.removeListener) {
       document.removeEventListener("scroll", this.handleScroll);
-      this.removeListener = true
+      this.removeListener = true;
     }
   }
+
   componentWillUnmount() {
-    if (!this.removeListener) {
+    if(!this.removeListener) {
       document.removeEventListener("scroll", this.handleScroll)
     }
   }
 
-
-
-  //处理屏幕滚动事件  实现加载更多的效果
+  // 处理屏幕滚动事件，实现加载更多的效果
   handleScroll = () => {
-    const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    const scrollTop = document.documentElement.scrollTop 
+    || document.body.scrollTop;
     const screenHeight = document.documentElement.clientHeight;
     const likeListTop = this.myRef.current.offsetTop;
     const likeListHeight = this.myRef.current.offsetHeight;
-    if (scrollTop >= likeListHeight + likeListTop - screenHeight) {
+    if(scrollTop >= likeListHeight + likeListTop - screenHeight) {
       const newData = this.state.data.concat(dataSource);
-      const newLoadTimes = this.state.loadTimes + 1
+      const newLoadTimes = this.state.loadTimes + 1;
       setTimeout(() => {
         this.setState({
           data: newData,
