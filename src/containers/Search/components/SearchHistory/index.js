33eb6aba80734f0ca0b051875ea95d40547b0c36
zhangjiaming <823600537@qq.com>
2020-06-03 18:28:19 +0800

    feat:搜索记录组件完成

---
+++ src/containers/Search/components/SearchHistory/index.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import "./style.css"
+
+
+class SearchHistory extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            data: ["烤鸭", "火锅", "面条"]
+        }
+    }
+    render() {
+        return (
+            <div className="searchHistory">
+                <div className="searchHistory__header">
+                    搜索记录
+                </div>
+                <ul className="searchHistory__list">
+                    {
+                        this.state.data.map((item, index) => {
+                            return <li key={index} className="searchHistory__item" onClick={this.handleClick}>
+                                {item}
+                            </li>
+                        })
+                    }
+                </ul>
+                <div className="searchHistory__clear" onClick={this.handleClear}>清楚搜索记录</div>
+            </div>
+        );
+    }
+
+
+    handleClick = () => {
+
+    }
+
+    handleClear = () => {
+        this.setState({
+
+            data: []
+        })
+    }
+}
+
+export default SearchHistory;
--- src/containers/Search/index.js
+++ src/containers/Search/index.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import SearchBox from './components/SearchBox'
-import PopularSearch from './components/PopularSearch'
+import SearchBox from './components/SearchBox';
+import PopularSearch from './components/PopularSearch';
+import SearchHistory from './components/SearchHistory';
 class Search extends Component {
     render() {
         return (
             <div>
                 <SearchBox />
                 <PopularSearch />
+                <SearchHistory/>
             </div>
         );
     }
