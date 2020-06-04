b16c2dfffcb9bfd5afa5e010a981887405072cde
zhangjiaming <823600537@qq.com>
2020-06-04 21:17:10 +0800

    feat:shoplist组件开发完成

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import { bindActionCreators } from "redux";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
-
 import { connect } from "react-redux";
 import ErrorToast from "../../components/ErrorToast";
 import { actions as appActions, getError } from "../../redux/modules/app";
 import Home from '../Home'
 import ProductDetail from "../ProductDeatil";
 import Search from "../Search"
+import SearchResult from "../SearchResult";
+
 
 class App extends Component {
   render() {
@@ -21,6 +22,7 @@ class App extends Component {
           <Switch>
             <Route path="/detail/:id" component={ProductDetail}></Route>
             <Route path="/search" component={Search}></Route>
+            <Route path="/search_result" component={SearchResult}></Route>
             <Route path="/" component={Home}></Route>
           </Switch>
         </Router>
---
+++ src/containers/SearchResult/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import ShopList from './components/ShopList';
+
+
+class SearchResult extends Component {
+    render() {
+        return (
+            <div>
+                <ShopList/>
+            </div>
+        );
+    }
+}
+
+export default SearchResult;
