13936444a84888f793def786c5b4f8e3705536bb
zhangjiaming <823600537@qq.com>
2020-06-08 00:17:55 +0800

    feat:keywordbox组件静态部分开发完成

--- src/containers/SearchResult/index.js
+++ src/containers/SearchResult/index.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
 import ShopList from './components/ShopList';
+import SearchHeader from './components/SearchHeader';
+import KeywordBox from './components/KeywordBox';
 
 
 class SearchResult extends Component {
     render() {
         return (
             <div>
-                <ShopList/>
+                <SearchHeader onBack={this.handleBack} onSearch={this.handleSearch} />
+                <KeywordBox  text='text'/>
+                <ShopList />
             </div>
         );
+    }
+
+
+    handleBack = () => {
+        this.props.history.push('/')
+    }
+
+    handleSearch = () => {
+        this.props.history.push('/search')
     }
 }
 
