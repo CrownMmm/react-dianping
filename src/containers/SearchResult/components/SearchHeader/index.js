390c47feeed4c22608ae05ab0d5792dac8f23e2c
zhangjiaming <823600537@qq.com>
2020-06-07 23:46:28 +0800

    feat:searchHeader组件静态部分开发完成

---
+++ src/containers/SearchResult/components/SearchHeader/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SearchHeader extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default SearchHeader;
