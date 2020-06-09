bab56e5ee5931f2b0cb39f11b9f04380e7230793
zhangjiaming <823600537@qq.com>
2020-06-10 01:10:51 +0800

    feat:OrderItem UserHeader Usermain组件开发完成

--- src/containers/User/index.js
+++ src/containers/User/index.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
+import UserMain from "./components/UserMain"
+import UserHeader from "./components/UserHeader"
 
 class User extends Component {
-    render() {
-        return (
-            <div>
-                个人中心
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <UserHeader onBack={this.handleBack} onLogout={this.handleLogout}/>
+        <UserMain/>
+      </div>
+    );
+  }
+
+  handleBack = () => {
+    // todo   
+  }
+
+  handleLogout = () => {
+    // todo
+  }
 }
 
 export default User;
